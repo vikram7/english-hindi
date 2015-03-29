1316dc6352d832d697868bc4c04e610561a6f01e
Vikram Ramakrishnan <lord.ezar@gmail.com>
2015-03-28 20:49:34 -0400

    add seeder for a few hundred words and meanings

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,18 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
 
-Category.create(part_of_speech: "noun")
-Category.create(part_of_speech: "verb")
-Category.create(part_of_speech: "adjective")
+# Category.create(part_of_speech: "noun")
+# Category.create(part_of_speech: "verb")
+# Category.create(part_of_speech: "adjective")
 
-Meaning.create(text: "squirrel", category_id: Category.find_by(part_of_speech: "noun").id)
-Meaning.create(text: "love", category_id: Category.find_by(part_of_speech: "noun").id)
-Meaning.create(text: "to love", category_id: Category.find_by(part_of_speech: "verb").id)
+# Meaning.create(text: "squirrel", category_id: Category.find_by(part_of_speech: "noun").id)
+# Meaning.create(text: "love", category_id: Category.find_by(part_of_speech: "noun").id)
+# Meaning.create(text: "to love", category_id: Category.find_by(part_of_speech: "verb").id)
 
-Word.create(text_hindi: "प्यार", text_romanized: "pyaar", meaning_id: 2, category_id: Meaning.find(2).category_id)
-Word.create(text_hindi: "इश्क", text_romanized: "ishq", meaning_id: 2, category_id: Meaning.find(2).category_id)
+# Word.create(text_hindi: "प्यार", text_romanized: "pyaar", meaning_id: 2, category_id: Meaning.find(2).category_id)
+# Word.create(text_hindi: "इश्क", text_romanized: "ishq", meaning_id: 2, category_id: Meaning.find(2).category_id)
+
+file = File.read("english-hindi-seed-file.csv")
+csv = CSV.parse(file, headers: true, header_converters: :symbol)
+count = 0
+
+csv.each do |row|
+  part_of_speech = row[:part_of_speech]
+  puts "Creating Category with a part of speech #{part_of_speech}"
+  category = Category.find_or_create_by!(part_of_speech: part_of_speech)
+
+  text = row[:text]
+  puts "Creating Meaning with text of #{text}"
+  meaning = Meaning.find_or_initialize_by(text: text)
+  meaning.category_id = category.id
+  meaning.save!
+
+  text_hindi = row[:text_hindi]
+  text_romanized = row[:text_romanized]
+  puts "Creating Word. Hindi: #{text_hindi}. Romanized: #{text_romanized}"
+  word = Word.find_or_initialize_by(text_hindi: text_hindi)
+  word.meaning_id = meaning.id
+  word.text_romanized = text_romanized
+  word.category_id = category.id
+  word.save!
+end
+
+
--- questions.md
+++ questions.md
@@ -4,3 +4,6 @@ What does "embed" ids mean?
 Why did I choose to make it an API?
 Are you happy with the Category - Meaning - Word relationships?
 What if you wanted to add another language like Tamil?
+Are there any other indexes I should add?
+Where is a good spot to put that csv file?
+There looks like there are a lot of database queries going on. What's the deal?
