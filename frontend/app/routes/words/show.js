c871319afdd1d596a474ff847e69486674da68d3
Vikram Ramakrishnan <lord.ezar@gmail.com>
2015-04-11 16:28:49 -0400

    handle both known and random words

--- frontend/app/routes/words/show.js
+++ frontend/app/routes/words/show.js
@@ -7,8 +7,6 @@ export default Ember.Route.extend({
     }
   },
   model: function(params) {
-    return this.store.find('word', { random: true }).then(function(randomWords){
-      return randomWords.get("firstObject");
-    });
+    return this.store.find('word', params.word_id);
   }
 });
